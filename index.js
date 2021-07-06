519642e367110417ea218b6fcdcc7ea789af9c5f
aleksandra.m <aleksandra.m@soft-artel.com>
2021-07-06 16:12:25 +0300

    simple web-server

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
+  if (req.method === "GET") {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
 
-  res.write('<h1>Hello from NodeJS</h1>')
-  res.end()
+    })
+    res.end(`
+      <h1>Form</h1>
+      <form method="post" action="/">
+        <input name="title" type="text" />
+        <button type="submit"> Submit </button>
+      </form>
+    `)
+  } else if (req.method === "POST") {
+    const body = []
+    res.writeHead('200', {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    req.on('data', data => {
+      body.push(Buffer.from(data))
+    })
+
+    req.on('end', () => {
+      const message = body.toString().split('=')[1]
+
+      res.end(`
+        <h1>Ваше сообщение: ${message} </h1>
+      `)
+    })
+
+  }
 })
 
 server.listen(3000, () => {
