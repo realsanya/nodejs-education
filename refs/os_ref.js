8b2dabb13abc35a5af74bf064fc5aaaeff7f1930
aleksandra.m <aleksandra.m@soft-artel.com>
2021-07-06 15:26:38 +0300

    module os

---
+++ refs/os_ref.js
@@ -0,0 +1,15 @@
+const os = require('os') // Operation System
+
+console.log(os.platform()) // Платформа
+
+console.log(os.arch()) // Архитектура
+
+console.log(os.cpus()) // Общая информация
+
+console.log(os.freemem()) // Свободная память
+
+console.log(os.totalmem()) // Сколько всего памяти
+
+console.log(os.homedir())// Корневая директория
+
+console.log(os.uptime()) // Сколько система работает
